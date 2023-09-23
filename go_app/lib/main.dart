b5276d25f5b473eca94f46cae0b38ddcbef30b8d
Praveen-prav <praveenpravee406@gmail.com>
2023-09-23 13:16:43 +0530

    Added Capture function and Display Screen

--- go_app/lib/main.dart
+++ go_app/lib/main.dart
@@ -1,7 +1,10 @@
 import 'dart:core';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import "package:camera/camera.dart";
 
+late String nameOwner, iD;
 Future<void> main() async {
   runApp(const MyApp());
 }
@@ -44,17 +47,108 @@ class _CamScreenState extends State<CamScreen> {
       appBar: AppBar(
         title: const Text("Take Picture"),
       ),
-      body: FutureBuilder<void>(
-        future: initconFut,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, display the preview.
-            return CameraPreview(controller);
-          } else {
-            // Otherwise, display a loading indicator.
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+      body: Column(
+        children: [
+          FutureBuilder<void>(
+            future: initconFut,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                // If the Future is complete, display the preview.
+                return CameraPreview(controller);
+              } else {
+                // Otherwise, display a loading indicator.
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+          const SizedBox(height: 16),
+          Container(
+            decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 251, 64, 232),
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            //
+            child: IconButton(
+                color: Colors.black,
+                onPressed: () async {
+                  try {
+                    await initconFut;
+                    final image = await controller.takePicture();
+                    if (!mounted) return;
+                    await Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => DisplayPictureScreen(
+                          imagePath: image.path,
+                        ),
+                      ),
+                    );
+                  } catch (e) {
+                    if (kDebugMode) {
+                      print(e);
+                    }
+                  }
+                },
+                icon: const Icon(Icons.camera_alt)),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Display the Picture')),
+      body: Column(
+        children: [
+          Image.file(File(imagePath)),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            "Owner name: $nameOwner",
+            style: const TextStyle(fontSize: 20),
+            textAlign: TextAlign.left,
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            "Unique ID of animal: $iD",
+            style: const TextStyle(fontSize: 20),
+            textAlign: TextAlign.left,
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+              colors: <Color>[
+                Color.fromARGB(255, 131, 13, 161),
+                Color.fromARGB(255, 210, 25, 207),
+                Color.fromARGB(255, 245, 66, 221),
+              ],
+            )),
+            child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.all(16.0),
+                  textStyle: const TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  if (kDebugMode) {
+                    print("upoload complete");
+                  }
+                },
+                child: const Text("Upload")),
+          ),
+        ],
       ),
     );
   }
@@ -87,7 +181,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late String nameOwner, iD;
   final myController = TextEditingController();
 
   @override
@@ -105,7 +198,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: Center(
             child: SizedBox(
-                height: 200,
+                height: 300,
                 width: 300,
                 child: Column(
                   children: [
@@ -113,9 +206,11 @@ class _MyHomePageState extends State<MyHomePage> {
                       decoration: const InputDecoration(
                           border: OutlineInputBorder(),
                           labelText: "Name of the Owner"),
-                      onSubmitted: (name) {
+                      onChanged: (name) {
                         nameOwner = name;
-                        print(nameOwner);
+                        if (kDebugMode) {
+                          print(nameOwner);
+                        }
                       },
                     ),
                     const SizedBox(height: 16),
@@ -124,9 +219,11 @@ class _MyHomePageState extends State<MyHomePage> {
                       decoration: const InputDecoration(
                           border: OutlineInputBorder(),
                           labelText: "Unique ID of animal"),
-                      onSubmitted: (text) {
+                      onChanged: (text) {
                         iD = text;
-                        print(iD);
+                        if (kDebugMode) {
+                          print(iD);
+                        }
                       },
                     ),
                     const SizedBox(height: 16),
@@ -137,8 +234,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           Color.fromARGB(255, 131, 13, 161),
                           Color.fromARGB(255, 210, 25, 207),
                           Color.fromARGB(255, 245, 66, 221),
-                        ]
-                        ,
+                        ],
                       )),
                       child: TextButton(
                           style: TextButton.styleFrom(
@@ -159,6 +255,7 @@ class _MyHomePageState extends State<MyHomePage> {
     WidgetsFlutterBinding.ensureInitialized();
     final cameras = await availableCameras();
     final firstCam = cameras.first;
+    if (!mounted) return;
     Navigator.push(
       context,
       MaterialPageRoute(
